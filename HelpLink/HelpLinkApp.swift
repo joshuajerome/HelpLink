de4ee97c590f7c9b7a214a780e2b4849f0a20a35
Soham Phadke <smphadke24@gmail.com>
2023-02-18 12:25:15 -0800

    Initial Commit

---
+++ HelpLink/HelpLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelpLinkApp.swift
+//  HelpLink
+//
+//  Created by Soham Phadke on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct HelpLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
