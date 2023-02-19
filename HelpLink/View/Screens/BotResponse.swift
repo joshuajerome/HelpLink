6c0fb055b4e96def8b702e911a4d9991e0d6b60f
Soham Phadke <smphadke24@gmail.com>
2023-02-18 19:41:51 -0800

    Added Diagnosis chatBot

---
+++ HelpLink/View/Screens/BotResponse.swift
@@ -0,0 +1,22 @@
+//
+//  BotResponse.swift
+//  HelpLink
+//
+//  Created by Soham Phadke on 2/18/23.
+//
+
+import Foundation
+
+func getBotResponse(message: String) -> String {
+    let tempMessage = message.lowercased()
+    
+    if tempMessage.contains("hello") {
+        return "Hey there!"
+    } else if tempMessage.contains("goodbye") {
+        return "Talk to you later!"
+    } else if tempMessage.contains("how are you") {
+        return "I'm fine, how about you?"
+    } else {
+        return "That's cool."
+    }
+}
